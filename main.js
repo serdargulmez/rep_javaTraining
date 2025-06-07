cbc098e711a0075c6597769ac7be88e76b5c8c92
Serdar Gülmez <serdar_gulmez@outlook.com>
2025-06-07 18:45:04 +0300

    +split, indexOf, substring methods were examined.

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 
-var ekp01 = "3";
-var isNotNum = isNaN(ekp01);
+// var ekp01 = "3";
+// var isNotNum = isNaN(ekp01);
 
-if (isNotNum) {
-    console.log("Variable is not Number and is: ", ekp01);
-} else {
-    console.log("Variable is Number: ",ekp01,isNotNum);
-}
+// if (isNotNum) {
+//     console.log("Variable is not Number and is: ", ekp01);
+// } else {
+//     console.log("Variable is Number: ",ekp01,isNotNum);
+// }
+
+// var ekp_01 = "This:3 is variables";
+
+// var ekp_02 = ekp_01.split(":");
+
+// console.log(ekp_02.at(2));
+
+// const searchTerm = ":";
+// var ekp_01 = "This:3 is variables";
+// var firstIndexOfSearchTerm = ekp_01.indexOf(searchTerm);
+// console.log("Index of search term is: " + firstIndexOfSearchTerm);
+
+// var ekp_02 = ekp_01.substring(firstIndexOfSearchTerm,firstIndexOfSearchTerm + 1);
+// console.log("substring result is: " + ekp_02);
 
