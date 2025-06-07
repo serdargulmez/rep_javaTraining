ec675f9c68f866a4665b3f8f64881b3ba896ffec
Serdar Gülmez <serdar_gulmez@outlook.com>
2025-06-07 12:31:01 +0300

    + Some code parts were tested.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var ekp01 = "3";
+var isNumber = isNaN(ekp01);
+
+if (!isNumber) {
+    console.log("Variable is Number.: " + ekp01);
+} else {
+    console.log("Variable is not Number!: " + ekp01);
+}
+
