8236ff7a8db2b7786390c9e5ba8868461985b31a
Serdar Gülmez <serdar_gulmez@outlook.com>
2025-06-07 12:36:22 +0300

    + Log prints were changed.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 var ekp01 = "3";
-var isNumber = isNaN(ekp01);
+var isNotNum = isNaN(ekp01);
 
-if (!isNumber) {
-    console.log("Variable is Number.: " + ekp01);
+if (isNotNum) {
+    console.log("Variable is not Number and is: ", ekp01);
 } else {
-    console.log("Variable is not Number!: " + ekp01);
+    console.log("Variable is Number: ",ekp01,isNotNum);
 }
 
